160704724df1d0b4369e95a9ce0bd2acfaf4b1d2
Daria <ms.darya2020@bk.ru>
2022-09-16 13:39:05 +0300

    add for

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 tickets = int(input('Сколько билетов Вы хотите приобрести? '))
-age = int(input('Сколько Вам лет? '))
-price = 0
-
-if age < 18:
-    print('Вход бесплатный')
-elif 18 <= age <= 25:
-    price = 990
-    print('Цена билета - 990 рублей')
-elif age > 25:
-    price = 1390
-    print('Цена билета - 1390 рублей')
-
-price *= tickets
+summ = 0
+for i in range(tickets):
+    age = int(input('Введите возраст посетителя № ' + str(i + 1) + ': '))
+    if age < 18:
+        print('Вход бесплатный')
+    elif 18 <= age <= 25:
+        summ += 990
+        print('Цена билета - 990 рублей')
+    elif age > 25:
+        summ += 1390
+        print('Цена билета - 1390 рублей')
+print('Стоимость билета - ', summ, 'рублей')
 if tickets >= 3:
-    price -= price * 10 / 100
-print('Стоимость билета - ', price, 'рублей')
+    summ -= summ * 10 / 100
+print('Стоимость билета со скидкой - ', summ, 'рублей')
